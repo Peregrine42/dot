14b730d4c67897930d97dd9369f14a42199a9420
Duncan Fermor <duncan@elitemail.org>
2015-12-26 13:21:14 +0000

    crazy query api complete

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,23 @@
 class DashboardsController < ApplicationController
   def show
-    tag = Tag.find_by(name: params[:id])
-    nodes = Link.where(tag_id: tag.id).map { |l| NormalizedNode.new(l) }
+    query = params[:query].split("/")
+    and_query = query[0].split("+")
+    filter_query = query[1..-1].to_a
+    nodes = and_query.map { |q|
+      tags = Tag.where(name: q)
+      tags.map { |tag|
+        Link.where(tag_id: tag.id)
+        .map { |l| NormalizedNode.new(l) }
+      }
+    }.flatten
+    filter_query.each do |f|
+    end
+    nodes = nodes.select { |n|
+      puts n.tags.values.inspect
+      puts filter_query.inspect
+      n.tags.values
+        .any? { |t| filter_query.include? t }
+    }
     @dashboard = Dashboard.new(nodes)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :dashboards
+  get "dashboards/*query", to: "dashboards#show"
 end
