e9fd46d5696e1ef56c6db1b8d59cabc270203f8a
Duncan Fermor <duncan@elitemail.org>
2015-12-26 12:43:35 +0000

    position based api change complete

--- app/models/normalized_node.rb
+++ app/models/normalized_node.rb
@@ -4,7 +4,9 @@ class NormalizedNode
   def initialize link
     node = link.node
     relevant_links = Link.where(node_id: node.id)
-    @tags = relevant_links.map { |l| l.tag.name }
+    @tags = relevant_links
+      .map { |l| [ l.position, l.tag.name ] }
+      .to_h
     @id = node.id
   end
 end
